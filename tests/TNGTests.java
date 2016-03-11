70f1fe6517fe2780fa5ca61bd102b415b77283c1
Sergey.Andreev <Sergey.Andreev@jetbrains.com>
2016-03-11 18:19:33 +0300

    tests added

--- tests/TNGTests.java
+++ tests/TNGTests.java
@@ -11,10 +11,30 @@ public class TNGTests {
 
     @Test
     public void test1() {
-        int[] testArr = {3, 4, 5, 6};
+        int[] testArr = {3, 4, 5};
         Triangle testTr = new Triangle();
         Assert.assertTrue("Разносторонний".equals(testTr.TypeOfTriangle(testArr)));
     }
 
+    @Test
+    public void test2() {
+        int[] testArr = {2, 4, 4};
+        Triangle testTr = new Triangle();
+        Assert.assertTrue("Равнобедренный".equals(testTr.TypeOfTriangle(testArr)));
+    }
+
+    @Test
+    public void test3() {
+        int[] testArr = {3, 3, 3};
+        Triangle testTr = new Triangle();
+        Assert.assertTrue("Равносторонний".equals(testTr.TypeOfTriangle(testArr)));
+    }
+
+    @Test
+    public void test4() {
+        int[] testArr = {1, -1, 1};
+        Triangle testTr = new Triangle();
+        Assert.assertFalse("Разносторонний".equals(testTr.TypeOfTriangle(testArr)));
+    }
 
 }
