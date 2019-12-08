621aeb3cdf92402ab82ed9e3762c6bc20a231752
smandreev <smandreev@gmail.com>
2019-12-08 14:30:55 +0300

    Order and data changed for Code Coverage demonstration sample

--- tests/TNGTests.java
+++ tests/TNGTests.java
@@ -5,6 +5,7 @@
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 public class TNGTests {
 
     @Test
@@ -14,19 +15,27 @@ public class TNGTests {
         Assert.assertEquals("Разносторонний", testTr.TypeOfTriangle(testArr));
     }
 
-    @Test
-    public void test2() {
-        int[] testArr = {2, 4, 4};
-        Triangle testTr = new Triangle();
-        Assert.assertEquals("Равнобедренный", testTr.TypeOfTriangle(testArr));
-    }
+//    @Test
+//    public void test5() {
+//        int[] testArr = {-1, 2, 1};
+//        Triangle testTr = new Triangle();
+//        Assert.assertTrue("Треугольник не существует".equals(testTr.TypeOfTriangle(testArr)), "Что-то пошло не так");
+//    }
+//
+//    @Test
+//    public void test3() {
+//        int[] testArr = {3, 3, 3};
+//        Triangle testTr = new Triangle();
+//        Assert.assertEquals("Равносторонний", testTr.TypeOfTriangle(testArr));
+//    }
+//
+//    @Test
+//    public void test2() {
+//        int[] testArr = {2, 4, 4};
+//        Triangle testTr = new Triangle();
+//        Assert.assertEquals("Равнобедренный", testTr.TypeOfTriangle(testArr));
+//    }
 
-    @Test
-    public void test3() {
-        int[] testArr = {3, 3, 3};
-        Triangle testTr = new Triangle();
-        Assert.assertEquals("Равносторонний", testTr.TypeOfTriangle(testArr));
-    }
 
 //    @Test
 //    public void test4() {
